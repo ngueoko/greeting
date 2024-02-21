7f65e58ff183d7ae4ceca4c41f30c1811d1b4d9b
ngueoko2016! <ngueokot@yahoo.fr>
2024-02-21 16:40:14 +0100

    remove white space

--- src/main/java/com/example/greeting/GreetingApplication.java
+++ src/main/java/com/example/greeting/GreetingApplication.java
@@ -13,10 +13,7 @@ public class GreetingApplication {
     @GetMapping("/greeting/{name}")
     public String greetingFunc(@PathVariable("name") String name){
         return "Greeting :"+name;
-
     }
-
-
     public static void main(String[] args) {
         SpringApplication.run(GreetingApplication.class, args);
     }
