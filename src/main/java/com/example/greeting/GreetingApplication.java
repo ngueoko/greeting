8bfa558909ce1cc8f52c7f637df8655348f1690e
ngueoko2016! <ngueokot@yahoo.fr>
2024-02-21 15:49:24 +0100

    Get /greeting/name

---
+++ src/main/java/com/example/greeting/GreetingApplication.java
@@ -0,0 +1,24 @@
+package com.example.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GreetingApplication {
+
+    @GetMapping("/greeting/{name}")
+    public String greetingFunc(@PathVariable("name") String name){
+        return "Greeting :"+name;
+
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingApplication.class, args);
+    }
+
+}
